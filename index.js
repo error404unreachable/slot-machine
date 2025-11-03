ad77b9f9f637759aa1b14ad4b6108dab04fa8764
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2025-11-03 14:16:49 -0600

    fixed syntax

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 const machine = document.getElementById("machine");
 const out = document.getElementById("out");
 let money = 0;
-let multiplier= 1;
+let multiplier = 1;
 
 function addMoney() {
-    money += (1 * multipier);
+    money += (1 * multiplier);
+    out.innerText = money;
     console.log(money);
 }

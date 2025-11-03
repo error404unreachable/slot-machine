05771a4e7b737ba162643633b7471ef98293c440
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2025-11-03 14:11:52 -0600

    Created money button and output elements

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 //index.js
+const machine = document.getElementById("machine");
+const out = document.getElementById("out");
+let money = 0;
+let multiplier= 1;
+
+function addMoney() {
+    money += (1 * multipier);
+    console.log(money);
+}
